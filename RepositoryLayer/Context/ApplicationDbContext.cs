26fdec9747746d96d3f5d009cac20cbc1e84dfe1
anas awaad <anas.shaban.awaad@gmail.com>
2024-09-01 14:25:14 +0300

    DbContext Setup

---
+++ RepositoryLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace RepositoryLayer.Context;
+public class ApplicationDbContext : DbContext
+{
+
+	// Constructor
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+	{
+	}
+
+	// Sql Tables
+	public DbSet<AboutUs> AboutUs { get; set; }
+	public DbSet<Category> Categories { get; set; }
+	public DbSet<ContactUs> ContactUs { get; set; }
+	public DbSet<HomePage> HomePages { get; set; }
+	public DbSet<Portfolio> Portfolios { get; set; }
+	public DbSet<Service> Services { get; set; }
+	public DbSet<SocialMedia> SocialMedias { get; set; }
+	public DbSet<Team> Teams { get; set; }
+	public DbSet<Testimonial> Testimonials { get; set; }
+
+
+	// Override for seeking assemblies
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+	}
+}
