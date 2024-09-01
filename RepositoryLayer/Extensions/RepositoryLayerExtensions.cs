c3dcd5d64b046083bca3b589d864183aba8f5e06
anas awaad <anas.shaban.awaad@gmail.com>
2024-09-01 17:13:18 +0300

    Repository Layer Extension & MS SQL Connection

---
+++ RepositoryLayer/Extensions/RepositoryLayerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RepositoryLayer.Context;
+
+namespace RepositoryLayer.Extensions;
+public static class RepositoryLayerExtensions
+{
+	public static IServiceCollection LoadRepositoryLayerExtensions(this IServiceCollection services, IConfiguration config)
+	{
+		// add connection string
+		services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+		return services;
+	}
+}
