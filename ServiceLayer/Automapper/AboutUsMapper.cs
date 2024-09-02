f4d0d2cf410c0f751d03358bed9417cfc0979e4c
anas awaad <anas.shaban.awaad@gmail.com>
2024-09-02 23:33:31 +0300

    add Automapper Configurations

---
+++ ServiceLayer/Automapper/AboutUsMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EntityLayer.Entities;
+using EntityLayer.ViewModels.AboutUsVM;
+namespace ServiceLayer.Automapper;
+public class AboutUsMapper:Profile
+{
+    public AboutUsMapper()
+    {
+        CreateMap<AboutUs, AboutUsListVM>();
+        CreateMap<AboutUs, AboutUsAddVM>();
+        CreateMap<AboutUs, AboutUsUpdateVM>();
+    }
+}
